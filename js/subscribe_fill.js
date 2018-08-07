07b415e410688e1dfbb9f8b6cc6d6a3f2eba8806
lf <1786978244@qq.com>
2018-08-07 08:12:04 +0800

    吴升级版

--- js/subscribe_fill.js
+++ js/subscribe_fill.js
@@ -9,4 +9,31 @@ $(function(){
 	    }
 	  ]
 	});
+
+	$('#showPicker').on('click', function () {
+        weui.picker([{
+            label: '业务1',
+            value: 0
+        }, {
+            label: '业务2',
+            value: 1
+        }, {
+            label: '业务3',
+            value: 2
+        },{
+            label: '业务4',
+            disabled: true,
+            value: 3
+        }, {
+            label: '其他',
+            value: 4
+        }], {
+            onChange: function (result) {
+                console.log(result);
+            },
+            onConfirm: function (result) {
+                console.log(result);
+            }
+        });
+    });
 })
